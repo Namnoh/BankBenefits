e24b4fff240d78363e543a40fd37fbdc9be2de58
Fernando Muñoz <fernandofailed@gmail.com>
2024-07-28 22:48:14 -0400

    ADD: Se agrega un header y footer básicos para la estructura del proyecto, además de la integración de angular material.

--- banks-benefits-app/tailwind.config.js
+++ banks-benefits-app/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#80BCBD',
+        secondary: '#AAD9BB',
+        tertiary: '#D5F0C1',
+        quaternary: '#F9F7C9'
+      },
+    },
   },
   plugins: [],
 }
